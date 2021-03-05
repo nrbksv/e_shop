df88d00f8bc0064264543ee8888dfa0604e352e3
nrbk <nrbksv@gmail.com>
2021-03-05 18:14:32 +0600

    Model 'Product' is registered in the admin-panel. Superuser created.

--- e_shop/shop/admin.py
+++ e_shop/shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from shop.models import Product
+
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category', 'product', 'price', 'balance']
+    list_filter = ['category']
+    search_fields = ['product', 'category']
+    fields = ['id', 'category', 'product', 'description', 'price', 'balance']
+    readonly_fields = ['id']
+
+
+admin.site.register(Product, ShopAdmin)
