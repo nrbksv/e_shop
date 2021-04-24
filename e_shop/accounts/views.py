e99c489a3628862d4ab246fb51c47df6cd3c68d1
nrbk <nrbksv@gmail.com>
2021-04-24 21:59:17 +0600

    Фикстуры обновлены.

--- e_shop/accounts/views.py
+++ e_shop/accounts/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import get_user_model
-from django.shortcuts import reverse
+from django.contrib.auth import get_user_model, login
+from django.shortcuts import reverse, redirect
 from django.views.generic import CreateView
 
 from accounts.forms import RegisterForm
@@ -10,5 +10,15 @@ class UserRegisterView(CreateView):
     form_class = RegisterForm
     model = get_user_model()
 
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
     def get_success_url(self):
-        return reverse('shop:product-list')
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = 'shop:product-list'
+        return next_url
