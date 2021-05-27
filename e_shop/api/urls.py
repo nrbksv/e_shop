7b528c088ad7a7e3ac7906d33e8bfdb2b625d1e8
nrbk <nrbksv@gmail.com>
2021-05-27 18:06:36 +0600

    Реализовано добавление, удаление, просмтотр товаров в корзине

--- e_shop/api/urls.py
+++ e_shop/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from api.views import ProductViewSet, OrderViewSet
+from api.views import ProductViewSet, OrderViewSet, Cart, get_token_view
 
 router = routers.DefaultRouter()
 router.register('products', ProductViewSet)
@@ -9,4 +9,6 @@ router.register('order', OrderViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('cart/', Cart.as_view(), name='cart-list'),
+    path('csrf/', get_token_view, name='csrf')
 ]
--- e_shop/api/views.py
+++ e_shop/api/views.py
@@ -1,7 +1,13 @@
+import json
+
+from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from api.serializers import ProductGetSerializer, OrderGetSerializer, OrderPostSerializer, OrderGetProductSerializer, \
+from api.serializers import ProductGetSerializer, OrderGetSerializer, OrderPostSerializer, \
     OrderPostProductSerializer, ProductPostSerializer
 from shop.models import Product, Order
 
@@ -47,3 +53,58 @@ class OrderViewSet(viewsets.ModelViewSet):
             else:
                 return Response(order_prods.errors, status=400)
         return Response(order.data)
+
+
+class Cart(APIView):
+
+    def get(self, request, *args, **kwargs):
+        cart = request.session.get('cart')
+        if cart:
+            json_cart = json.dumps(cart)
+            return HttpResponse(json_cart)
+        return JsonResponse({'error': 'cart is empty'}, status=400)
+
+    def post(self, request, *args, **kwargs):
+        if not request.session.get('cart'):
+            request.session['cart'] = dict()
+        cart = request.session.get('cart')
+        if request.body:
+            data = json.loads(request.body)
+            product = get_object_or_404(Product, id=data['product'])
+            if product.balance - data['quantity'] >= 0:
+                if not cart.get(str(product.id)):
+                    cart[str(data['product'])] = data['quantity']
+                else:
+                    cart[str(data['product'])] += data['quantity']
+                request.session['cart'] = cart
+                return JsonResponse(data)
+            return JsonResponse({'error': f'Ordered: {data["quantity"]} In stock: {product.balance}'}, status=400)
+        return JsonResponse({'error': 'no data'}, status=400)
+
+    def delete(self, request, *args, **kwargs):
+        cart = request.session.get('cart')
+        if request.body:
+            data = json.loads(request.body)
+            if cart:
+                if cart.get(str(data['product'])):
+                    balance = cart[str(data['product'])] - data['quantity']
+                    if balance <= 0:
+                        del cart[str(data['product'])]
+                    cart[str(data['product'])] = balance
+                    request.session['cart'] = cart
+                    return JsonResponse({'response': 'ok'})
+                return JsonResponse({'error': 'no such product'}, status=400)
+        return JsonResponse({'error': 'no data'}, status=400)
+
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse({'aaa': 'asdasd'})
+    return HttpResponseNotAllowed('Only GET request are allowed')
+
+
+
+
+
+
